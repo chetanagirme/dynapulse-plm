5934756f4f8a6a454e810b2d5d992ebefd09df66
Chetana Girme <chetnagirme@gmail.com>
2025-12-05 14:30:55 +0530

    Fix server crash and deploy all features

--- server/index.js
+++ server/index.js
@@ -13,6 +13,23 @@ app.use(cors());
 app.use(express.json());
 
 // Database Connection
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('MongoDB connection error:', err));
+
+// Routes
+app.use('/api/products', require('./routes/products'));
+app.use('/api/boms', require('./routes/boms'));
+app.use('/api/suppliers', require('./routes/suppliers'));
+app.use('/api/ecos', require('./routes/ecos'));
+app.use('/api/quality', require('./routes/quality'));
+app.use('/api/audit', require('./routes/audit'));
+
+app.get('/', (req, res) => {
+    res.send('Antigravity PLM API is running');
+});
+
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
